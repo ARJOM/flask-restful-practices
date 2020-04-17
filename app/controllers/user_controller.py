74c1849bb32667b2fc5862e6095069c46827eaf1
Ricart <ricarteusuo11@gmail.com>
2020-04-17 19:41:22 -0300

    feat: add user CRUD

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,6 +1,58 @@
-from flask_restful import Resource
+from flask import request
+from flask_restful import Resource, reqparse
+from app.database.connection import get_db
+
+parser = reqparse.RequestParser()
+parser.add_argument('username')
+parser.add_argument('password')
 
 
-class HelloWorld(Resource):
+class User(Resource):
+    def get(self, user_id):
+        cur = get_db().cursor()
+        cur.execute(f"SELECT username FROM users WHERE id={user_id}")
+        user = cur.fetchone()
+        cur.close()
+
+        return user
+
+    def delete(self, user_id):
+        cur = get_db().cursor()
+        cur.execute(f"DELETE FROM users WHERE id={user_id}")
+        get_db().commit()
+        cur.close()
+
+    def put(self, user_id):
+        data = parser.parse_args()
+        username = data['username']
+        password = data['password']
+
+        cur = get_db().cursor()
+        cur.execute(f"UPDATE users SET username='{username}', password='{password}' WHERE id={user_id}")
+        get_db().commit()
+        cur.close()
+
+        return 201
+
+
+class UserList(Resource):
     def get(self):
-        return {'hello': 'world'}
+        cur = get_db().cursor()
+        cur.execute("SELECT * FROM users")
+        users = cur.fetchall()
+        cur.close()
+
+        return users
+
+    def post(self):
+        data = parser.parse_args()
+        username = data['username']
+        password = data['password']
+
+        cur = get_db().cursor()
+        cur.execute(f"INSERT INTO users(username, password)"
+                    f"VALUES ('{username}','{password}')")
+        get_db().commit()
+        cur.close()
+
+        return 201
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,4 +1,5 @@
 from app import api
 from app.controllers.user_controller import *
 
-api.add_resource(HelloWorld, '/home')
+api.add_resource(UserList, '/users')
+api.add_resource(User, '/users/<int:user_id>')
